daf7046049f7ac7b71881b2bebb98525b2a401ec
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-04 22:53:17 -0700

    making changes

--- Drapchaty_Matthew_Output_Assignment/js/script.js
+++ Drapchaty_Matthew_Output_Assignment/js/script.js
@@ -1,7 +1,11 @@
 alert("hello");
 
-var favoriteNumber = 4;
-var favoriteColor = 
+var favoriteNumber = 4;  // assigning a number value
+
+var favoriteColor = ("Green");
+
+
 console.log("This is my favorite number!");
 console.log(favoriteNumber);
-
+console.log("This is my favorite color!");
+console.log(favoriteColor);
