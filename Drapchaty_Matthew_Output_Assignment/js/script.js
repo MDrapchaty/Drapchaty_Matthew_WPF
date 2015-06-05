bc96e7871c3687d8c4d231ae0711f8233d2aae10
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-04 23:11:42 -0700

    more changes

--- Drapchaty_Matthew_Output_Assignment/js/script.js
+++ Drapchaty_Matthew_Output_Assignment/js/script.js
@@ -1,11 +1,16 @@
-alert("hello");
-
+alert("Sorry for being late!");
+// Drapchaty, Matthew  SDI section 01 MDV2330-O   assignment: Output Assignment
 var favoriteNumber = 4;  // assigning a number value
 
-var favoriteColor = ("Green");
+var favoriteColor = ("Green"); // assigning a string value
+
+var enjoysSnowboarding = true; // Boolean Variable
 
 
-console.log("This is my favorite number!");
-console.log(favoriteNumber);
-console.log("This is my favorite color!");
-console.log(favoriteColor);
+console.log("This is my favorite number!"); // shows message to give reason to favoriteNumber variable
+console.log(favoriteNumber); // output of number value
+console.log("This is my favorite color!"); // message to give reason to favoriteColor varibale
+console.log(favoriteColor); // output of string variable
+console.log("I enjoy snowboarding?"); // message for boolean output
+console.log(enjoysSnowboarding); // output for boolean variable
+
