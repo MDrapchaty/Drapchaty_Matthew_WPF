15a28e0cdfc5cd3af12387ea49e8fd2260e7d430
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-10 01:14:57 -0700

    expressions_rectangle

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,3 +1,10 @@
-/**
- * Created by drapchaty on 6/10/15.
+/* Drapchaty, Matthew
+   Expressions_worksheet
+   6/10/2015
  */
+// calculate the area of a given rectangle
+var height = 10; // setting number for height variable
+var width = 20; // setting number for width variable
+var area = height * width; //equation for area using number variables
+var result = area; //setting result to solved equation
+console.log(result); // output of solved equation
