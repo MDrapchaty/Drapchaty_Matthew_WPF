e8ab44582ea6d91ec7ddd7c92c55fde349c9c470
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-10 01:45:35 -0700

    slices

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,11 +2,23 @@
    Expressions_worksheet
    6/10/2015
  */
-  //compare dog years to human years
-  //multiply human years by 7 to get answer
+  //dog years
+  //multiply human years by 7 to get dog years
 var humanYears = 3; // setting human years variable
 var sparkysAge = 7 * humanYears; //setting dog (sparky) years variable
 
 console.log("Sparky is " + humanYears +" human years old, which is " + sparkysAge + " in dog years." );
+// output for answer of the equation concatenated w/ string data to read easily
+
+
+//slice of pie part1
+
+var slices = 8; //setting number of slices per pizza variable
+var people = 21; //setting number of people at the party variable
+var pizzas = 9; //setting number of pizzas ordered variable
+
+//multiply slices * pizzas to get total number of slices. Divide that number by poeple to get answer.
+var result = slices * pizzas / people; //equation to get result
+console.log(result);
 
 
