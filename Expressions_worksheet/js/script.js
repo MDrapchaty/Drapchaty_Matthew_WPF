99ebea7d393a91d49930fb31bdf8875499406972
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-10 02:13:03 -0700

    grocery

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -11,14 +11,28 @@ console.log("Sparky is " + humanYears +" human years old, which is " + sparkysAg
 // output for answer of the equation concatenated w/ string data to read easily
 
 
-//slice of pie part1
+//slice of pie part 1
 
 var slices = 8; //setting number of slices per pizza variable
-var people = 21; //setting number of people at the party variable
-var pizzas = 9; //setting number of pizzas ordered variable
+var people = 10; //setting number of people at the party variable
+var pizzas = 4; //setting number of pizzas ordered variable
 
 //multiply slices * pizzas to get total number of slices. Divide that number by poeple to get answer.
 var result = slices * pizzas / people; //equation to get result
-console.log(result);
+console.log("Each person ate " + result + " slices of pizza at the party."); // output of answer, easily read
+
+
+//slice of pie part 2
+
+var remainder = slices * pizzas % people; // multiply slices and pizzas, and use the modulo sign with people to get the remaining slices
+console.log("Sparky got " + remainder + " slice of pizza."); //output of answer so it can be read
+
+
+//average shopping bill
+
+var spending = [144, 179, 122, 104, 217]; // setting an array of spending cost results as a variable
+var totalSpending = spending[0] + spending[1] + spending[2] + spending[3] + spending[4];
+console.log(totalSpending);
+
 
 
