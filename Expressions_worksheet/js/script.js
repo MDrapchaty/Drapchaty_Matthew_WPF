947a91c1f87a9714e488a243e6956ad7d116dc05
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-10 01:29:38 -0700

    sparkys age

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,9 +2,11 @@
    Expressions_worksheet
    6/10/2015
  */
-// calculate the area of a given rectangle
-var height = 10; // setting number for height variable
-var width = 20; // setting number for width variable
-var area = height * width; //equation for area using number variables
-var result = area; //setting result to solved equation
-console.log(result); // output of solved equation
+  //compare dog years to human years
+  //multiply human years by 7 to get answer
+var humanYears = 3; // setting human years variable
+var sparkysAge = 7 * humanYears; //setting dog (sparky) years variable
+
+console.log("Sparky is " + humanYears +" human years old, which is " + sparkysAge + " in dog years." );
+
+
