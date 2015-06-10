5511b135e2e7ac05b58b000a42ac736117b97e2a
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-09 20:29:11 -0700

    %

--- Examples/js/script.js
+++ Examples/js/script.js
@@ -1,8 +1,5 @@
 //expressions
 
-//area of the triangle is half of the width times the height
-var width = 4;
-var height = 5;
-var area = (width * height)/2;
-console.log(area);
+var remainder = 33 % 2;
+console.log(remainder);
 
