192b0216e788ea683a3907bf6f7b6897f329bb53
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-16 19:43:56 -0700

    conditionals

---
+++ Conditionals/js/script.js
@@ -0,0 +1,32 @@
+//Conditional logic - else if
+
+/*var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+//if the price of the phone is less our budget AND if our paycheck is over 300
+if(iPhonePrice < budget || wonLottery){
+console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
+}
+*/
+
+//var gpa = 48;
+
+//if the gpa is over the min 2.0 score the student can graduate
+/*if( gpa > 2.0){
+    console.log("You can graduate!");
+}else{
+    console.log("GPA is too low!")
+}*/
+
+//(gpa > 2.0) ? console.log("you can graduate!") : console.log("GPA is too low!");
+
+var age = 11;
+var book;
+
+//If the child is under 10 they get green eggs and ham, otherwise they get the time machine
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
+
