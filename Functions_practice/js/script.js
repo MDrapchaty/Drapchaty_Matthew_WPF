9028fb7c6092c25fd5599e25c16d07dd05458467
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-23 01:00:42 -0700

    functions practice

---
+++ Functions_practice/js/script.js
@@ -0,0 +1,33 @@
+// functions - function execution
+
+/* function outptMsg(){
+    console.log("Hello World");
+}
+ var width = 5;
+function calcArea(){
+    var width = 20;
+    var height =30;
+    var area = width * height;
+    //console.log(area);
+}
+console.log(width);
+calcArea();
+calcArea();
+calcArea();
+ */
+
+//calcArea(30, 20);
+
+
+/* function calcArea(w, h){
+    var area = w * h;
+    console.log(area);
+} */
+function dogYears(age){
+
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.")
+}
+dogYears(4);
+dogYears(5);
+
