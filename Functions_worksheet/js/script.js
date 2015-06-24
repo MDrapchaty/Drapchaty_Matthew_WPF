2c73d461fa73843afa83862600fcb063693d05cf
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-23 19:57:27 -0700

    circumference

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,1 +1,20 @@
-console.log("Test); 
+/* Drapchaty, Matthew
+    6/23/15
+    Functions_worksheet */
+
+
+//calculate the circumference of a circle
+
+//vars
+var radius = 11; //creating variable for radius
+
+//function
+
+function calcCirc(radius) { //function for how to find circumference equation, with parameter named radius
+    var circum = 2 * 3.14 * radius;  //creating a variable to store result
+    return circum; // returning result to variable circum
+}
+
+//main code
+var circumference = calcCirc(radius); //calling function using argument
+console.log(circumference); //print out using returned variable result
