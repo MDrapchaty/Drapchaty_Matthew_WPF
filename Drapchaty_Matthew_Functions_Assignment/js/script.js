18e2595deb380aeffec16fe3952112b0a8da9a40
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-24 00:17:49 -0700

    working on ring

--- Drapchaty_Matthew_Functions_Assignment/js/script.js
+++ Drapchaty_Matthew_Functions_Assignment/js/script.js
@@ -1,1 +1,22 @@
-console.log("hello");
+/* Drapchaty, Matthew
+    Functions Assignment
+    6/23/2015  */
+//Florida lottery has six random numbers between 1 and 53; the Powerball lottery has five random numbers between 1 and 59 and a powerball between 1 and 35
+
+//variables
+
+
+
+
+//functions
+function randomNumGen(min, max){
+    var florida = Math.random() * (max-min) + min;
+    florida = Math.round(florida);
+
+    return florida;
+}
+
+
+//main code
+
+randomNumGen(1, 53);
