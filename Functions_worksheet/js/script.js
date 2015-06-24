49d91c1ae0e004c9329ced1ddfabccf5ad91fb1b
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-06-23 20:05:23 -0700

    bee stings

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -17,4 +17,23 @@ function calcCirc(radius) { //function for how to find circumference equation, w
 
 //main code
 var circumference = calcCirc(radius); //calling function using argument
-console.log(circumference); //print out using returned variable result
+console.log("The circumference of the circle is " + circumference + "."); //print out using returned variable result
+
+
+
+//stung
+
+
+//vars
+var victimWeight = 185;  //variable for animals weight in lbs
+
+
+//function
+function deathByStings(weight) { //creating function, and parameter
+    var result = weight * 8.666666667; //creating variable to store result
+    return result; // returning result
+}
+
+//main code
+var stings = deathByStings(victimWeight); // creating var to return result to, and calling function
+console.log("It takes " + stings + " bee stings to kill this animal.");
